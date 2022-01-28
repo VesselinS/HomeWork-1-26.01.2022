97e1597ca963fbc702ed6005e4481b32cdd4b8e9
VesselinS <vesselin.i.stoyanov@gmail.com>
2022-01-28 16:13:40 +0200

    Initial commit

---
+++ src/main/java/qa/automation/HomeWork.java
@@ -0,0 +1,36 @@
+package qa.automation;
+
+import java.util.Scanner;
+
+public class HomeWork {
+
+    // psvm includes the below command
+    public static void main(String[] args) {
+
+        //writes the text in the console
+        System.out.println("Please enter your first name:");
+
+        //Scanner is class for reading values from the console
+        Scanner scanner = new Scanner(System.in);
+
+        //a string variable that will store the value entered from the user
+        String firstName = scanner.next();
+
+        //writes the text in the console
+        System.out.println("Please enter your last name:");
+
+        //a string variable that will store the value entered from the user
+        String lastName = scanner.next();
+
+        //writes the text in the console
+        System.out.println("Please enter your hobby:");
+
+        //a string variable that will store the value entered from the user
+        String hobby = scanner.next();
+
+        // sout includes the below command
+         System.out.println(firstName + " " + lastName + " has a hobby " + hobby + "!");
+
+    }
+
+}
