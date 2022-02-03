ec06752516e0d49234f0582cdfa41b3bf4919f95
VesselinS <vesselin.i.stoyanov@gmail.com>
2022-02-03 19:09:44 +0200

    New changes

--- src/main/java/qa/automation/HomeWork.java
+++ src/main/java/qa/automation/HomeWork.java
@@ -14,19 +14,19 @@ public class HomeWork {
         Scanner scanner = new Scanner(System.in);
 
         //a string variable that will store the value entered from the user
-        String firstName = scanner.next();
+        String firstName = scanner.nextLine();
 
         //writes the text in the console
         System.out.println("Please enter your last name:");
 
         //a string variable that will store the value entered from the user
-        String lastName = scanner.next();
+        String lastName = scanner.nextLine();
 
         //writes the text in the console
         System.out.println("Please enter your hobby:");
 
         //a string variable that will store the value entered from the user
-        String hobby = scanner.next();
+        String hobby = scanner.nextLine();
 
         // sout includes the below command
          System.out.println(firstName + " " + lastName + " has a hobby " + hobby + "!");
